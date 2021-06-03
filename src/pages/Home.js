172516f18904cca59160fefee8eef8c9a83ab99d
성현Maria 김 <globalkmaria@gmail.com>
2021-06-03 21:18:59 +0900

    book loading margin update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import ShowSearchContainer from '../container/ShowSearchContainer';
 import useInfiniteScroll from '../hook/useInfiniteScroll';
 import { fetchBookList } from '../modules/books';
 import { useSelector } from 'react-redux';
-
+import './pages.scss';
 function Home() {
   const { query, data } = useSelector((state) => state.books);
   useInfiniteScroll(query, fetchBookList);
